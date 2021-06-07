5eee9e1cea317a42112725bc14b8a68d49eff449
romanalikevich <roma.alikevich@mail.ru>
2021-06-07 22:48:21 +0300

    add: 3 task

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,15 +3,21 @@ import Foundation
 public extension Int {
     
     var roman: String? {
+        
+        if self <= 0 || self > 3999 {
+            return nil
+        }
         var number = self
         var result = ""
-        let romanaStorage: [Int:String] = [1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"]
+        let array = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+                let romanSymbolsArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         
         while(number > 0) {
-            for (index, value) in romanaStorage.enumerated() {
-                if number - index > 0 {
-                    result += romanaStorage[index]!
-                    number -= index
+            for (index, value) in array.enumerated() {
+                if number - value >= 0 {
+                    number -= value
+                    result += romanSymbolsArray[index]
+                    break
                 }
             }
         }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,28 @@ import Foundation
 
 final class FillWithColor {
     
-    func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
-    }
+    func fill(_ image: inout [[Int]], _ row: Int, _ column: Int, _ color: Int, _ newColor: Int) {
+            if row >= image.count ||  column >= image[0].count || row < 0 || column < 0 || image[row][column] != color || image[row][column] == newColor {
+                return
+            }
+            
+            image[row][column] = newColor
+            
+        fill(&image, row - 1, column, color, newColor)
+        fill(&image, row + 1, column, color, newColor)
+        fill(&image, row, column - 1, color, newColor)
+        fill(&image, row, column + 1, color, newColor)
+        }
+        
+        func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
+            if image.count == 0 || row >= image.count || column >= image[0].count || row < 0 || column < 0 {
+                return image
+            }
+            
+            var image = image
+            let color = image[row][column]
+            
+            fill(&image, row, column, color, newColor)
+            return image
+        }
 }
