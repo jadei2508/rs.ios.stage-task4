73cf7c1c972847e20fb12bd242d7cab8ccbf7e1e
Roman <68894436+jadei2508@users.noreply.github.com>
2021-06-07 19:27:16 +0300

    add: solution of romanian numbers

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,18 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        var number = self
+        var result = ""
+        let romanaStorage: [Int:String] = [1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"]
+        
+        while(number > 0) {
+            for (index, value) in romanaStorage.enumerated() {
+                if number - index > 0 {
+                    result += romanaStorage[index]!
+                    number -= index
+                }
+            }
+        }
+        return result
     }
 }
